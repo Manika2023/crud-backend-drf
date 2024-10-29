b48726ee3308a17a31aefe2f1e8c0be05ff300e4
manika <singhmanika409@gmail.com>
2024-10-29 20:46:26 +0530

    add allowed_hosts

--- crud/settings.py
+++ crud/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c24dk+4v5*$qsdt^uc#g#q263&h6pglr#!=9b(9-6n1ru$fen8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
